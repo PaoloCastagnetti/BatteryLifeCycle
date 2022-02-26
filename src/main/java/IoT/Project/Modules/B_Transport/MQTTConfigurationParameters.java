6101e26f517b0091746ffc6f9cce2e09f184a4ae
Marco <marcosavarese@live.it>
2022-02-26 12:21:53 +0100

    Module B - Added all configuration data and descriptors of all the elements

--- src/main/java/IoT/Project/Modules/B_Transport/MQTTConfigurationParameters.java
+++ src/main/java/IoT/Project/Modules/B_Transport/MQTTConfigurationParameters.java
@@ -1,4 +1,5 @@
 package IoT.Project.Modules.B_Transport;
+
 /**
  * @author Paolo Castagnetti, 267731@studenti.unimore.it
  * @project IoT-BatteryLifeCycle
@@ -8,11 +9,11 @@ public class MQTTConfigurationParameters {
     //Do not touch
     public static String BROKER_ADDRESS = "155.185.228.20";
     public static int BROKER_PORT = 7883;
-    public static final String MQTT_USERNAME="267731@studenti.unimore.it";
-    public static final String MQTT_PASSWORD="gzfwajdvhoyjtytm";
+    public static final String MQTT_USERNAME="271055@studenti.unimore.it";
+    public static final String MQTT_PASSWORD="fvvnivmzjltosnks";
     public static final String MQTT_BASIC_TOPIC= String.format("/iot/user/%s", MQTT_USERNAME);
     //Editable
-    public static final String MQTT_NUMBER="267731";
+    public static final String MQTT_NUMBER="271055";
     public static final String SENSOR_TOPIC="sensor";
     public static final String VEHICLE_TOPIC="vehicle";
     public static final String LOCATION_TOPIC="GPSlocation";
--- src/main/java/IoT/Project/Modules/B_Transport/Models/VehicleTelemetryData.java
+++ src/main/java/IoT/Project/Modules/B_Transport/Models/VehicleTelemetryData.java
@@ -28,6 +28,7 @@ public class VehicleTelemetryData {
         this.timestamp = timestamp;
     }
 
+    //Measurments updater via random numbers
     public void updateMeasurments(){
         if(this.random == null){
             this.random = new Random(System.currentTimeMillis());
@@ -36,6 +37,53 @@ public class VehicleTelemetryData {
         double randomlongitude = 15.27 + this.random.nextDouble() * 10.0;
 
         this.gpsTrackerDescriptor = new GPSTrackerDescriptor(randomlatitude, randomlongitude, 1232.0);
+        this.enginetemp = 80 + this.random.nextDouble() * 10.0;
+        this.batterylevel = this.batterylevel - (this.random.nextDouble() * 10.0);
+        this.speed = 10 + this.random.nextInt() * 90;
+        this.timestamp = System.currentTimeMillis();
+    }
 
+    //Getters and Setters
+    public GPSTrackerDescriptor getGpsTrackerDescriptor() {
+        return gpsTrackerDescriptor;
+    }
+    public void setGpsTrackerDescriptor(GPSTrackerDescriptor gpsTrackerDescriptor) {
+        this.gpsTrackerDescriptor = gpsTrackerDescriptor;
+    }
+    public double getBatterylevel() {
+        return batterylevel;
+    }
+    public void setBatterylevel(double batterylevel) {
+        this.batterylevel = batterylevel;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    public double getEnginetemp() {
+        return enginetemp;
+    }
+    public void setEnginetemp(double enginetemp) {
+        this.enginetemp = enginetemp;
+    }
+    public long getTimestamp() {
+        return timestamp;
+    }
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    //toString Method
+    @Override
+    public String toString(){
+        StringBuffer VehicleTelemetry = new StringBuffer("VehicleTelemetry{");
+        VehicleTelemetry.append("Geolocation: ").append(gpsTrackerDescriptor);
+        VehicleTelemetry.append(" Battery Level: ").append(batterylevel).append('%');
+        VehicleTelemetry.append(" Engine Temperature: ").append(enginetemp).append('°');
+        VehicleTelemetry.append(" Timestamp: ").append(timestamp);
+        VehicleTelemetry.append('}');
+        return VehicleTelemetry.toString();
     }
 }
