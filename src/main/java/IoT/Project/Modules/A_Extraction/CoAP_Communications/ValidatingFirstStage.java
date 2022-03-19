b8d58cfe3319cb074afb3f2c95b9d87221c4b2fc
PaoloCastagnetti <castagnettipaolo01@gmail.com>
2022-03-19 17:30:39 +0100

    Prove fallite, aggiunti dettagli irrilevanti.

--- src/main/java/IoT/Project/Modules/A_Extraction/CoAP_Communications/ValidatingFirstStage.java
+++ src/main/java/IoT/Project/Modules/A_Extraction/CoAP_Communications/ValidatingFirstStage.java
@@ -1,8 +1,10 @@
 package IoT.Project.Modules.A_Extraction.CoAP_Communications;
 
+import IoT.Project.DCPM.DCPM;
 import IoT.Project.Modules.A_Extraction.Models.UploadingActuatorDescriptor;
 import com.google.gson.Gson;
 import org.eclipse.californium.core.CoapClient;
+import org.eclipse.californium.core.CoapResource;
 import org.eclipse.californium.core.CoapResponse;
 import org.eclipse.californium.core.Utils;
 import org.eclipse.californium.core.coap.CoAP;
@@ -20,28 +22,30 @@ public class ValidatingFirstStage {
     private static Gson gson = new Gson();
 
     public static void sendResource(UploadingActuatorDescriptor UAD){
+
         CoapClient coapClient = new CoapClient(COAP_ENDPOINT);
         UAD.simulateOriginOfMaterial();
-        //Request Class is a generic CoAP message: in this case we want a GET.
-        //"Message ID", "Token" and other header's fields can be set
-        Request request = new Request(CoAP.Code.PUT);
 
-        //Set Request as Confirmable
+
+        //PUT
+        Request request = new Request(CoAP.Code.PUT);
         request.setConfirmable(true);
         String payload = gson.toJson(UAD);
         request.setPayload(payload.getBytes());
 
         System.out.printf("Request Pretty Print: \n%s%n", Utils.prettyPrint(request));
-
         try {
             CoapResponse coapResp = coapClient.advanced(request);
             System.out.printf("Response Pretty Print: \n%s%n", Utils.prettyPrint(coapResp));
         } catch (ConnectorException | IOException e) {
             e.printStackTrace();
         }
+
+        //GET
         Request req = new Request(CoAP.Code.GET);
         req.setConfirmable(true);
 
+        System.out.printf("Request Pretty Print: \n%s%n", Utils.prettyPrint(req));
         try{
             CoapResponse resp = coapClient.advanced(req);
             System.out.printf("Response Pretty Print: \n%s%n", Utils.prettyPrint(resp));
